a5a9ecbce5c1ec358e42f7e252d2508ec452c5ec
Michael Mansy <michael.mansy@yahoo.ca>
2019-08-09 18:24:04 -0400

    style the height of images

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -4,21 +4,27 @@ class ImageCard extends React.Component{
     constructor(props){
         super(props);
 
+        this.state = { spans : 0};
+
         this.imageRef = React.createRef();
     }
 
-    componentDidMount(){
+    componentDidMount(){   //adding event listener waiting for the image to load
         this.imageRef.current.addEventListener('load', this.setSpans);
     }
 
     setSpans = () => {
-        console.log(this.imageRef.current.clientHeight);
+        const height = this.imageRef.current.clientHeight;
+
+        const spans = Math.ceil(height / 150);   //finding out how many spans does an image need
+
+        this.setState = ({ spans});   //setting state to match the number of spans needed
     };
 
     render() {
         const {description, urls} = this.props.image;
         return (
-            <div>
+            <div style={{ gridRowEnd: `span ${this.state.spans}`}}>
                 <img 
                     ref={this.imageRef}
                     alt={description}
